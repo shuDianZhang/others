0a94e4943e87f85e9bf7a10b68c53a4e25c6baea
StreamLights <506975676@qq.com>
2017-09-27 23:41:20 +0800

    增加了promise解决回调地狱

--- async/index.js
+++ async/index.js
@@ -16,7 +16,7 @@ function moveTo(obj, direction, distance, cb) {
             speed = direction === 'right' ? 5 : -5;
         var target = posX + distance;              //目标位置
         var timer = '';
-        timer = setInterval(function() {
+        timer = setInterval(function () {
             posX = posX + speed;
             moveBox.style.left = posX + 'px';
             if (posX === target) {
@@ -29,7 +29,7 @@ function moveTo(obj, direction, distance, cb) {
             speed = direction === 'bottom' ? 5 : -5;
         var target = posY + distance;
         var timer = '';
-        timer = setInterval(function() {
+        timer = setInterval(function () {
             posY = posY + speed;
             moveBox.style.top = posY + 'px';
             if (posY === target) {
@@ -42,24 +42,67 @@ function moveTo(obj, direction, distance, cb) {
     }
 }
 
-moveBox.addEventListener('click', function() {
+// moveBox.addEventListener('click', function() {
 
-    moveTo(moveBox, 'bottom', 400, function() {
+//     moveTo(moveBox, 'bottom', 400, function() {
 
-        moveTo(moveBox, 'right', 500, function() {
+//         moveTo(moveBox, 'right', 500, function() {
 
-            moveTo(moveBox, 'top', 400, function() {
+//             moveTo(moveBox, 'top', 400, function() {
 
-                moveTo(moveBox, 'left', 500, function() {
+//                 moveTo(moveBox, 'left', 500, function() {
 
-                    alert('跑完一圈啦~~~~~~~~');
+//                     alert('跑完一圈啦~~~~~~~~');
 
-                });
+//                 });
 
-            });
+//             });
+//         });
+
+//     });
+
+
+// }, false);
+
+
+
+//使用promise对象解决回调地狱
+//优点： 异步的嵌套转变成同步的执行
+//缺点： 参数太复杂，影响阅读体验
+
+
+
+function movePromise() {
+    return new Promise(function (resolve) {
+        moveTo(moveBox, 'bottom', 500, function () {
+            resolve();
         });
-
     });
+}
+moveBox.addEventListener('click', function () {
+    movePromise()
+        .then(function () {
+            return new Promise(function (resolve) {
+                moveTo(moveBox, 'right', 500, function () {
+                    resolve();
+                });
+            });
+        })
+        .then(function () {
+            return new Promise(function (resolve) {
+                moveTo(moveBox, 'top', 500, function () {
+                    resolve();
+                });
+            });
+        })
+        .then(function () {
+            return new Promise(function (resolve) {
+                moveTo(moveBox, 'left', 500, function () {
+                    resolve();
+                });
+            });
+        })
+});
 
 
-}, false);
+//最优雅的回掉解决方案 async/await
